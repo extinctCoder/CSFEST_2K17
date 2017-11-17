c9d9fed3c85a91a8d0a9a5ebe3d22fb405b7ea8c
darthVader <extinctCoder@outlook.com>
2017-11-18 01:35:12 +0600

    before datagrid

---
+++ SmartHomeAutomation/SmartHomeAutomation/AdminControls/DatabasePanel.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SmartHomeAutomation.db;
+
+namespace SmartHomeAutomation.AdminControls
+{
+    /// <summary>
+    /// Interaction logic for DatabasePanel.xaml
+    /// </summary>
+    public partial class DatabasePanel : UserControl
+    {
+        public DatabasePanel()
+        {
+            InitializeComponent();
+            using (var db = new HomeAutomationCentralDatabaseEntities())
+            {
+                this.DataGrid.ItemsSource = db.Users.ToList();
+            }
+        }
+    }
+}
