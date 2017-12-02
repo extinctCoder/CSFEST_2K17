239884537d5dbd2194b482bf42fb675a141c462e
ghostaiub <33343723+ghostaiub@users.noreply.github.com>
2017-12-02 23:32:52 +0600

    login.php added

---
+++ SmartHomeAutomation_Himu/Login.php
@@ -0,0 +1,97 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<table>
+		<div>
+			<div>
+				<table align="center" width="100%" border="1">
+					<tr align="right">
+						<td width="30%">
+							<!--<a href="Home.html"><img src="images/logo.png" align="left"></a>-->
+						</td>
+						<td width="30%">&nbsp;</td>
+						<td>
+							<a href="Home.html" >Home <img src="images/home.png"></a>
+						</td>
+						<td width="10%">
+							<a href="Registration.html">Registration<img src="images/registration.png"></a>
+						</td>
+						<td width="10%">
+							<a href="Login.html">Login<img src="images/login.png"></a>
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<div>
+				Please Login with your username and password
+			</div>
+       
+			<div>
+				<fieldset>
+					<legend>Login</legend>
+					<table align="center">
+						<tr>
+							<td><label>User Name</label></td>
+							<td>:</td>
+							<td><input type="name" name="name"/></td>
+							<td><img src="images/person.png"></td>
+						</tr>
+						<tr>
+							<td><label>Password</label></td>
+							<td>:</td>
+							<td><input type="password" name="password"/></td>
+							<td><img src="images/password.png"></td>
+						</tr>
+						<tr>
+							<td align="center" colspan="4">
+								<input type="submit" name="submit" value="Login">
+								<input type="reset" name="reset" value="Reset">
+						</tr>
+						<tr>
+							<td colspan="4" align="center">
+								<a href="ForgetPassword.html">Forget Password</a>
+								<a href="Registration.html">Sign Up
+							</td>
+						</tr>
+					</table>
+				</fieldset>
+			</div>
+
+			<div>
+				<table align="center">
+					<tr align="center">
+						<td>
+							<a href="https://www.facebook.com/">
+								<img src="images/facebook.png">
+							</a>
+						</td>
+						<td>
+							<a href="https://www.twitter.com/">
+								<img src="images/twitter.png">
+							</a>
+						</td>
+					</tr>
+				</table>
+				
+				<table align="center">
+					<tr>
+						<td align="center" colspan="3">
+							<a href="About_Us.html">About Us   </a>
+						</td>
+						<td align="center" colspan="3">
+							<a href="Contact_Us.html">Contact Us   </a>
+						</td>
+						<td align="center" colspan="3">
+							<a href="privacyPolicy.html">Privacy Policy   </a>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</table>
+    </table>
+</div>
